1545b87a60013e9e07db867f93f2c5829ab5850d
Yetanotherx <yetanotherx@gmail.com>
2011-04-04 09:46:33 -0400

    Add plugin

---
+++ src/com/roboticoverlord/micro/bukkit/MinecartStarterVehicleListener.java
@@ -0,0 +1,22 @@
+package com.roboticoverlord.micro.bukkit;
+
+import org.bukkit.entity.Minecart;
+import org.bukkit.event.vehicle.VehicleExitEvent;
+import org.bukkit.event.vehicle.VehicleListener;
+
+public class MinecartStarterVehicleListener extends VehicleListener {
+
+    private MinecartStarter parent;
+    
+    public MinecartStarterVehicleListener(MinecartStarter parent) {
+	this.parent = parent;
+    }
+
+    @Override
+    public void onVehicleExit(VehicleExitEvent e) {
+	if(e.getVehicle() instanceof Minecart)
+	    if(parent.mcarts.contains(e.getVehicle().getEntityId())) {
+		e.getVehicle().remove();
+	    }
+    }
+}
